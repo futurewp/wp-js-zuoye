e3965e398d93d83891648c45e4a03fa8b9fce5ac
futurewp <1154944904@qq.com>
2017-09-24 23:28:02 +0800

    2017.09.24

--- 天猫/js/2.js
+++ 天猫/js/2.js
@@ -2,7 +2,7 @@
 * @Author: Administrator
 * @Date:   2017-09-19 17:33:08
 * @Last Modified by:   Administrator
-* @Last Modified time: 2017-09-19 22:10:30
+* @Last Modified time: 2017-09-24 18:12:53
 */
 window.onload=function(){
 	let aside=document.getElementsByClassName('banner-left')[0];
@@ -22,8 +22,12 @@ window.onload=function(){
     let banner=document.getElementsByClassName('banner1')[0];
 	let bar=document.getElementsByClassName('bar-list')[0];
 	let list=bar.getElementsByTagName('li');
-	let barr=document.getElementsByClassName('banner-img');
-
+    let barrBox=document.getElementsByClassName('banner-list')[0];
+    let barrli=barrBox.getElementsByTagName('li');
+	let barr=barrBox.getElementsByClassName('banner-img');
+    let imgw=parseInt(getComputedStyle(barrBox,null).width);
+    let now=0;
+    let next=0;
     let t=setInterval(move, 3000);
     //时间函数----自动轮播
     banner.onmouseover = function(){
@@ -32,40 +36,92 @@ window.onload=function(){
     banner.onmouseout = function(){
     	t=setInterval(move, 3000);
     }
-    
-
-    let num=0; //记录窗口显示的下标
     function move(){
-    	num++;
-    	if(num == barr.length){
-    		num=0; 
-    	}
-    	for(let i=0;i<barr.length;i++){
-    		barr[i].style.display = 'none';
-    		list[i].style.background='#a2a2a2'
-
-    	}
-        barr[num].style.display = 'block';
-        list[num].style.background='#fff'
-
+        next++;
+        if(next==barrli.length){
+            next=0;
+        }
+        list[now].style.background = '#a2a2a2';
+        list[next].style.background = '#fff';
+        barrli[next].style.left = imgw+'px';
+        animate(barrli[now],{left:-imgw});
+        animate(barrli[next],{left:0});
+        now=next;
     }
-    //鼠标移入
-	for(let i=0;i<list.length;i++){
-		list[i].onmouseover=function(){
-			//所有的消失（遍历），对应的显示
-			for(let i=0;i<list.length;i++){
-				barr[i].style.display='none';
-                list[i].style.background='#a2a2a2';
-			}
-			barr[i].style.display='block';	
-			list[i].style.background='#fff';	
-			num=i;
-		}
-	}
 
 
+    //鼠标点击
+	for(let i=0;i<barrli.length;i++){
+        list[i].onclick=function(){
+            if(now==i){return;}
+            list[now].style.background = '#a2a2a2';
+            list[i].style.background = '#fff';
+            barrli[i].style.left = imgw+'px';
+            animate(barrli[now],{left:-imgw});
+            animate(barrli[i],{left:0});
+            now=next=i;
+        }
+    }
 
+//按需加载  楼层跳转
+    let byleft=document.querySelectorAll('.bkleft>li>a');
+    let ch=innerHeight;
+    let floor=document.querySelectorAll('.floor');
+    let floorArr=[];
+    let color = ['#64c333','#ff6700','#ea5f8d','#0aa6e8','#64c333','#f15453','#19c8a9','#0aa6e8','#ea5f8d']
+    floor.forEach(element=>{
+        floorArr.push(element.offsetTop)
+    })
+    let flag=true;
+        console.log(floorArr);
+    window.onscroll = function(){
+        if(!flag){
+            return;
+        }
+        // console.log(document.documentElement.scrollTop)
+        // console.log(document.body.scrollTop)
+        let scrolltop=document.documentElement.scrollTop;
+        floorArr.forEach((value,index)=>{
+            if(scrolltop+ch>=value+200){
 
+                for(let i=0;i<byleft.length;i++){
+                    byleft[i].style.background='none';
+                }
+                 byleft[index].style.background=color[index];
+
+                //index
+                let imgs=floor[index].getElementsByTagName('img');
+                // console.log(imgs)
+                for(let i=0;i<imgs.length;i++){
+                    imgs[i].src = imgs[i].getAttribute('imgpath');
+                }
+            }
+        })
+    }
+
+    byleft.forEach((element,index)=>{
+        element.onclick=function(){
+            for(let i=0;i<byleft.length;i++){
+                byleft[i].style.background='none';
+            }
+               element.style.background=color[index];
+            
+             flag=false;
+           // document.documentElement.scrollTop=floorArr[index];
+            animate(document.documentElement,{scrollTop:floorArr[index]},function(){
+                flag=true;
+            })
+        }
+
+    })
+        // byleft.onmouseover=function(){
+        //     byleft[index].style.background=color[index];
+        //     byleft[byleft.length-1].style.background='#999';
+        // }
+        // byleft.onmouseout=function(){
+        //     byleft[index].style.background='rgba(0,0,0,0.6)';
+        //     byleft[byleft.length-1].style.background='#999';
+        // }
 
 
 
--- 小米/js/xm.js
+++ 小米/js/xm.js
@@ -2,7 +2,7 @@
 * @Author: Administrator
 * @Date:   2017-09-19 19:21:44
 * @Last Modified by:   Administrator
-* @Last Modified time: 2017-09-19 22:08:06
+* @Last Modified time: 2017-09-24 22:17:00
 */
 window.onload=function(){
 	let aside=document.getElementsByClassName('word')[0];
@@ -23,79 +23,193 @@ window.onload=function(){
     let banner=document.getElementsByClassName('banner')[0];
 	let bar=document.getElementsByClassName('bar-list')[0];
 	let list=bar.getElementsByTagName('li');
-	let barr=document.getElementsByClassName('banner-img');
+    let barrBox=document.getElementsByClassName('banner-list')[0];
+    let barrli=barrBox.getElementsByTagName('li');
+	let barr=barrBox.getElementsByClassName('banner-img');
+    // console.log(barr);
 	let lefts=document.getElementsByClassName('banner-left')[0];
 	let rights=document.getElementsByClassName('banner-right')[0];
-    //左右箭头
+    let imgw=parseInt(getComputedStyle(barrBox,null).width);
+    // console.log(imgw);
+    let now=0;
+    let next=0;
+    let flag=true;
+    // 左右箭头
     rights.onclick = function(){
-          move()
+        if (!flag) {
+            return;
+        }
+          move();
+          flag=false;
     }
     lefts.onclick = function(){
-    	  moveL()
+        if (!flag) {
+            return;
+        }
+          moveL();
+          flag=false;
+    	 
     }
-    //时间函数----自动轮播
-    let t=setInterval(move, 1000);
+    // 时间函数----自动轮播
+    let t=setInterval(move,3000);
     
     banner.onmouseover = function(){
     	clearInterval(t);
     }
     banner.onmouseout = function(){
-    	t=setInterval(move, 1000);
+    	t=setInterval(move, 3000);
     }
     
-    let num=0; //记录窗口显示的下标
     function move(){
-    	num++;
-    	if(num == barr.length){
-    		num=0; 
-    	}
-    	for(let i=0;i<barr.length;i++){
-    		barr[i].style.display = 'none';
-    		list[i].style.background='#555171'
-
-    	}
-        barr[num].style.display = 'block';
-        list[num].style.background='#abc9fa'
-
+        next++;
+        if(next==barrli.length){
+            next=0;
+        }
+        list[now].style.background = '#555171';
+        list[next].style.background = '#abc9fa';
+        barrli[next].style.left = imgw+'px';
+        animate(barrli[now],{left:-imgw});
+        animate(barrli[next],{left:0},function(){
+            flag=true;
+        });
+        now=next;
     }
-    //左点击
     function moveL(){
-    	num--;
-    	if(num <0){
-    		num=barr.length-1; 
-    	}
-    	for(let i=0;i<barr.length;i++){
-    		barr[i].style.display = 'none';
-    		list[i].style.background='#555171'
-
-    	}
-        barr[num].style.display = 'block';
-        list[num].style.background='#abc9fa'
-
+        next--;
+        if(next<0){
+            next=barrli.length-1;
+        }
+        list[now].style.background = '#555171';
+        list[next].style.background = '#abc9fa';
+        barrli[next].style.left = -imgw+'px';
+        animate(barrli[now],{left:imgw});
+        animate(barrli[next],{left:0},function(){
+            flag=true;
+        });
+        now=next;
     }
+
+    for(let i=0;i<barrli.length;i++){
+        list[i].onclick=function(){
+            if(now==i){return;}
+            list[now].style.background = '#555171';
+            list[i].style.background = '#abc9fa';
+            barrli[i].style.left = imgw+'px';
+            animate(barrli[now],{left:-imgw});
+            animate(barrli[i],{left:0});
+            now=next=i;
+        }
+    }
+
+
+//明星产品
+    // let starBox=document.getElementsByClassName('sb-bottom')[0];
+    // let star=document.getElementsByClassName('star-box-bottom')[0];
+    // let starli=star.getElementsByTagName('li')[0];
+    // let sLefts=document.getElementsByClassName('star-box-top-jiantou1')[0];
+    // let sRights=document.getElementsByClassName('star-box-top-jiantou2')[0];
+    // let sw=(starli.offsetWidth+parseInt(getComputedStyle(starli,null).marginRight))*5
+    // let i=0;
+    // console.log(sw);
+
+    // sLefts.onclick=function(){
+    //     if(i==0){
+    //         i++;
+    //     }
+    //     star.style.transform=`translateX(-${i*sw}px)`;
+    //     sLefts.style.color=`#e0e0e0`;
+    //     sRights.style.color=`#666666`;
+    //     sRights.onmouseover=function(){
+    //         // sLefts.style.color=`#e0e0e0`;
+    //         sRights.style.color=`#ff6700`;
+    //     }
+    //     // sRights.onmouseout=function(){
+    //     //     // sLefts.style.color=`#e0e0e0`;
+    //     //     sRights.style.color=`#666666`;
+    //     // }
+    // }
+    // sRights.onclick=function(){
+    //     if(i==1){
+    //         i--;
+    //     }
+    //     star.style.transform=`translateX(-${i*sw}px)`;
+    //     sLefts.style.color=`#666666`;
+    //     sRights.style.color=`#e0e0e0`;
+
+    //     sLefts.onmouseover=function(){
+    //         sLefts.style.color=`#ff6700`;
+    //         // sRights.style.color=`#e0e0e0`;
+    //     }
+    //     // sLefts.onmouseout=function(){
+    //     //     sLefts.style.color=`#666666`;
+    //     //     // sRights.style.color=`#e0e0e0`;
+    //     // }
+    // }
+    // setInterval(function(){
+    //     // animate(star,{left:0})
+    //     star.style.transform='translateX(0px)';
+    //     sLefts.style.color=`#666666`;
+    //     sRights.style.color=`#e0e0e0`;
+    // },3000);
+    // setInterval(function(){
+    //     // animate(star,{left:-sw})
+    //     star.style.transform='translateX(-1240px)';
+    //     sLefts.style.color=`#e0e0e0`;
+    //     sRights.style.color=`#666666`;
+    // },3000); 
+  let star=document.querySelector('.star-box-bottom');
+  let zuohua=document.querySelector('.star-box-top-jiantou1');
+  let youhua=document.querySelector('.star-box-top-jiantou2');
+  let num=0;
+  setInterval(function(){
+    if(num==2){
+      num=0;
+    }
+  star.style.marginLeft=-num*1226+'px';
+  star.style.transition='all 1s';
+  num++;
+  },3000);
+  youhua.onclick=function(){
+    if(num===1){
+      return;
+    }
+    // clearInterval(starT);
+    zuohua.style.color=`#666666`;
+    youhua.style.color=`#e0e0e0`;
+    star.style.marginLeft=-num*1226+'px';
+    num++;
+  }
+  zuohua.onclick=function(){
+    if(num===0){
+         return;
+    }
+    // clearInterval(starT);
+    zuohua.style.color=`#e0e0e0`;
+    youhua.style.color=`#666666`;
+    star.style.marginLeft=-num*1226+'px';
+    num--;
+  }
+
    
-    //鼠标移入
-    for(let i=0;i<5;i++){
-        list[i].onmouseover=function(){
-               list[i].style.background='#abc9fa';  
-            }
-        list[i].onmouseout=function(){
-               list[i].style.background='#555171';
-            }       
-    }
-    //鼠标点击
-	for(let i=0;i<5;i++){
-		list[i].onclick=function(){
-			for(let i=0;i<5;i++){
-				barr[i].style.display='none';
-                list[i].style.background='#555171';
-			}
-			barr[i].style.display='block';	
-			list[i].style.background='#abc9fa';	
-			num=i;
-		}
 
-	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
